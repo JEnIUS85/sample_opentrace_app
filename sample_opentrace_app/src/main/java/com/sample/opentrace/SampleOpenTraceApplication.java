a1b8c06ac87939966e209a78ed6869f27ea030cf
JEnIUS85 <nitinkumarsharma.perf@gmail.com>
2019-02-20 15:29:27 +0530

    Updated with Tracer Bean

--- sample_opentrace_app/src/main/java/com/sample/opentrace/SampleOpenTraceApplication.java
+++ sample_opentrace_app/src/main/java/com/sample/opentrace/SampleOpenTraceApplication.java
@@ -1,7 +1,7 @@
 package com.sample.opentrace;
 
 import io.jaegertracing.Configuration;
-import io.jaegertracing.internal.samplers.ProbabilisticSampler;
+import io.jaegertracing.internal.samplers.ConstSampler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -11,10 +11,27 @@ public class SampleOpenTraceApplication {
 
     @Bean
     public io.opentracing.Tracer jaegerTracer(){
-        return new Configuration("helloService").getTracer();
-    //, new Configuration.SamplerConfiguration(),
-        //                new Configuration.ReporterConfiguration()
+       // return new Configuration("helloService").getTracer();
 
+        Configuration.SamplerConfiguration samplerConfiguration = new Configuration.SamplerConfiguration()
+                .withType(ConstSampler.TYPE)
+                .withParam(1);
+
+        Configuration.SenderConfiguration senderConfiguration = new Configuration.SenderConfiguration()
+                .withEndpoint("http://localhost:14268/api/traces");
+
+        Configuration.ReporterConfiguration reporterConfiguration = new Configuration.ReporterConfiguration()
+                .withLogSpans(true)
+                .withFlushInterval(1000)
+                .withMaxQueueSize(1000)
+                .withSender(senderConfiguration);
+
+        return new Configuration("HelloService")
+                .withSampler(samplerConfiguration)
+                .withReporter(reporterConfiguration)
+                .getTracer();
+
+		
         //return Configuration.fromEnv().getTracer();
     }
     public static void main(String [] args){
